7ec503ff6828c494fe8030d41bb104e7a84b9c2a
naaila24sia <naaila24si@mahasiswa.pcr.ac.id>
2026-04-28 09:14:36 +0700

    P6 Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,47 @@
+import React, { Suspense } from "react";
 import "./assets/tailwind.css";
-import Dashboard from "./pages/Dashboard";
-import Orders from "./pages/Orders";
-import Customers from "./pages/Customers";
-import NotFound from "./pages/NotFound";
-import Error400 from "./pages/Error400";
-import Error401 from "./pages/Error401";
-import Error403 from "./pages/Error403";
-import AddOrders from "./pages/AddOrders";
-import AddCustomers from "./pages/AddCustomers";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import AddOrder from "./pages/AddOrders";
-import MainLayout from "./layouts/MainLayout";
-import AuthLayout from "./layouts/AuthLayout";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Forgot from "./pages/auth/Forgot";
+import Loading from "./components/Loading";
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/customers" element={<Customers />} />
-        <Route path="/400" element={<Error400 />} />
-        <Route path="/401" element={<Error401 />} />
-        <Route path="/403" element={<Error403 />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/customers/add" element={<AddCustomers />} />
-        <Route path="/orders/add" element={<AddOrders />} />
-      </Route>
+  const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+  const Customers = React.lazy(() => import("./pages/Customers"));
+  const Orders = React.lazy(() => import("./pages/Orders"));
+  const NotFound = React.lazy(() => import("./pages/NotFound"));
+  const Error400 = React.lazy(() => import("./pages/Error400"));
+  const Error401 = React.lazy(() => import("./pages/Error401"));
+  const Error403 = React.lazy(() => import("./pages/Error403"));
+  const AddCustomers = React.lazy(() => import("./pages/AddCustomers"));
+  const AddOrders = React.lazy(() => import("./pages/AddOrders"));
+  const Login = React.lazy(() => import("./pages/auth/Login"));
+  const Forgot = React.lazy(() => import("./pages/auth/Forgot"));
+  const Register = React.lazy(() => import("./pages/auth/Register"));
+  const MainLayout = React.lazy(() => import("./layouts/MainLayout"));
+  const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"));
 
-      <Route element={<AuthLayout/>}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="/forgot" element={<Forgot/>} />
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/400" element={<Error400 />} />
+          <Route path="/401" element={<Error401 />} />
+          <Route path="/403" element={<Error403 />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/customers/add" element={<AddCustomers />} />
+          <Route path="/orders/add" element={<AddOrders />} />
         </Route>
-    </Routes>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
